59e836139a389d15dbb484516747642f9292ff82
Trang <nguyenminhtrang9213@gmail.com>
2025-10-27 23:28:21 +0100

    add components

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from "react";
+
+function Navbar(): JSX.Element {
+  return (
+    <nav className="bg-emerald-100">
+      <div className="mx-auto max-w-7xl px-8 py-4 flex"></div>
+    </nav>
+  );
+}
+
+export default Navbar;
